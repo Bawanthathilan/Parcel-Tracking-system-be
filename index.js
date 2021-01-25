55d0323cf1775375625c20c784282d232c309174
bawanthathilan <bawantharathnayaka@gmail.com>
2021-01-25 12:01:09 +0530

    HTTP post req handling

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 import express from 'express';
+import models from './models/index.js';
 
 const port = 3000;
 const app = express();
 
+app.use(express.json());
+
+const log = (msg) => console.log(msg);
+app.get('/' , (req,res)=>{
+    res.send('hello viewers' + req.params.id)
+})
+
+app.post('/' , (req,res)=>{
+    const body = req.body;
+    res.send('hello viewers this is post' + body.message);
+})
+
 app.listen(port, ()=>{
     console.log("Listening to port" + port);
 });
+
+log(models);
