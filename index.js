85ae820d18a47eb822f642045cf9d826296ae3ce
bawanthathilan <bawantharathnayaka@gmail.com>
2021-01-26 15:44:39 +0530

    save user to db

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+
 import express from 'express';
 import models from './models/index.js';
+import mongoose from 'mongoose';
+
+
 
 const port = process.env.PORT || 3000;
 const app = express();
@@ -7,13 +11,35 @@ const app = express();
 app.use(express.json());
 
 const log = (msg) => console.log(msg);
+
+const uri = "mongodb://localhost:27017/parcelapi";
+const options = {};
+
+const connectWithDb = ()=>{
+    mongoose.connect(uri , options,(err , db)=>{
+        if(err){
+            console.error(err);
+        }
+        else log("database connection established");
+    });
+};
+
+connectWithDb();
+
+
 app.get('/' , (req,res)=>{
     res.send('parcel tracking api')
 })
 
 app.post('/' , (req,res)=>{
     const body = req.body;
-    res.send('hello viewers this is post' + body.message);
+    const user = new models.User({username: body.username , createdAt: new Date()});
+    user.save().then((savedUser)=>{
+        res.status(201).send('User saved Id' + savedUser._id);
+    }).catch((error)=>{
+        res.status(500).send(error);
+    });
+    
 })
 
 app.listen(port, ()=>{
