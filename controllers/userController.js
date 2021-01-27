38efcb9477223d17ef4dcf4f41405763f263f12d
bawanthathilan <bawantharathnayaka@gmail.com>
2021-01-27 14:26:10 +0530

    add user controller

---
+++ controllers/userController.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import models from './models/index.js';
+const router = express.Router();
+
+const getHandler = (req,res) =>{
+    res.send('hello viewers' + req.query.id);
+};
+
+const postHandler = (req,res)=>{
+    const body = req.body;
+    const user = new models.User({username: body.username , createdAt: new Date()});
+    user.save().then((savedUser)=>{
+        res.status(201).send('User saved Id' + savedUser._id);
+    }).catch((error)=>{
+        res.status(500).send(error);
+    });
+    
+}
+
+router.get('/' , getHandler);
+router.post('/' , postHandler);
--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 
 import express from 'express';
-import models from './models/index.js';
+
 import mongoose from 'mongoose';
 import connectWithDb from './mongo';
-
 
 
 const port = process.env.PORT || 3000;
@@ -11,26 +10,9 @@ const app = express();
 
 app.use(express.json());
 const log = (msg) => console.log(msg);
+
 connectWithDb();
-
-
-app.get('/' , (req,res)=>{
-    res.send('parcel tracking api')
-})
-
-app.post('/' , (req,res)=>{
-    const body = req.body;
-    const user = new models.User({username: body.username , createdAt: new Date()});
-    user.save().then((savedUser)=>{
-        res.status(201).send('User saved Id' + savedUser._id);
-    }).catch((error)=>{
-        res.status(500).send(error);
-    });
-    
-})
 
 app.listen(port, ()=>{
     console.log("Listening to port" + port);
 });
-
-log(models);
